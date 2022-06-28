4f51869f58216702ec7895d1f412e940647e994e
Kaleb Elwert <belak@coded.io>
2022-06-27 23:17:17 -0700

    Pull schemes directly from GitHub for CI

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"compress/gzip"
 	"embed"
 	"flag"
 	"io/fs"
+	"net/http"
 	"os"
 
+	"github.com/nlepage/go-tarfs"
 	"github.com/sirupsen/logrus"
 )
 
@@ -15,6 +18,7 @@ var schemesFS embed.FS
 var (
 	schemesDir  string
 	templateDir string
+	onlineMode  bool
 
 	// Define the logger we'll be using
 	logVerbose bool
@@ -31,6 +35,7 @@ func init() {
 	flag.StringVar(&schemesDir, "schemes-dir", "-", "Target directory for scheme data. The default value uses internal schemes.")
 	flag.StringVar(&templateDir, "template-dir", ".", "Target template directory to build.")
 	flag.BoolVar(&logVerbose, "verbose", false, "Log all debug messages")
+	flag.BoolVar(&onlineMode, "online", false, "Run in online mode and pull schemes directly from GitHub")
 
 	rawLog.Level = logrus.InfoLevel
 	if logVerbose {
@@ -38,7 +43,32 @@ func init() {
 	}
 }
 
+func getSchemesFromGithub() (fs.FS, error) {
+	log.Info("Attempting to load schemes from GitHub")
+
+	r, err := http.Get("https://github.com/base16-project/base16-schemes/archive/refs/heads/main.tar.gz")
+	if err != nil {
+		return nil, err
+	}
+
+	gzipReader, err := gzip.NewReader(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	targetFS, err := tarfs.New(gzipReader)
+	if err != nil {
+		return nil, err
+	}
+
+	// The archive has a subfolder containing all the schemes, so we return a
+	// subfs of the folder.
+	return fs.Sub(targetFS, "base16-schemes-main")
+}
+
 func main() {
+	var err error
+
 	flag.Parse()
 
 	log.WithFields(logrus.Fields{
@@ -49,8 +79,17 @@ func main() {
 
 	var targetFS fs.FS
 	if schemesDir == "-" {
-		log.Info("Using internal schemes")
-		targetFS, _ = fs.Sub(schemesFS, "schemes")
+		// If we're in online mode, the default is to pull from GitHub,
+		// otherwise use the embedded schemes.
+		if onlineMode {
+			targetFS, err = getSchemesFromGithub()
+			if err != nil {
+				log.WithError(err).Fatal("Failed to load schemes from GitHub")
+			}
+		} else {
+			log.Info("Using internal schemes")
+			targetFS, _ = fs.Sub(schemesFS, "schemes")
+		}
 	} else {
 		log.Infof("Processing scheme dir %q", schemesDir)
 		targetFS = os.DirFS(schemesDir)
